6742c03f3c1b05ad4a26d3ae6eecd246060e3d92
root <root@ip-172-31-26-169.us-west-2.compute.internal>
2018-05-06 18:00:57 +0000

    Add second instance for remote state testing

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -60,7 +60,26 @@ resource "aws_instance" "test-instance1" {
 
 }
 
+resource "aws_instance" "test-instance2" {
+        ami = "ami-4e79ed36"
+        instance_type = "t2.micro"
+        vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+        user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p "${var.server_port}" &
+        EOF
+
+        tags {
+              Name = "test-webserver-02"
+             }
+
+}
+
+
 output "public_ip" {
 	value = "${aws_instance.test-instance1.public_ip}"
 }
+
 
