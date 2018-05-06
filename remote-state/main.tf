ffd6f7646762b3aa4599904c5541923f41d54faa
Timam <me@timam.io>
2018-05-06 23:50:45 +0600

    timam created one instence

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -26,3 +26,41 @@ resource "aws_s3_bucket" "terraform_state" {
 output "s3_bucket_arn" {
     value = "${aws_s3_bucket.terraform_state.arn}"
 }
+
+
+variable "server_port" {
+	description = "The port the server will use for HTTP requests"
+	default = 8080
+}
+
+resource "aws_security_group" "instance" {
+	name = "terraform-sg"
+	ingress {
+		from_port = "${var.server_port}"
+		to_port = "${var.server_port}"
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+		}
+}
+
+resource "aws_instance" "test-instance1" {
+	ami = "ami-4e79ed36"
+	instance_type = "t2.micro"
+	vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+	
+	user_data = <<-EOF
+	#!/bin/bash
+	echo "Hello, World" > index.html
+	nohup busybox httpd -f -p "${var.server_port}" &
+	EOF
+	
+	tags {
+	      Name = "test-webserver-01"
+	     }
+
+}
+
+output "public_ip" {
+	value = "${aws_instance.test-instance1.public_ip}"
+}
+
