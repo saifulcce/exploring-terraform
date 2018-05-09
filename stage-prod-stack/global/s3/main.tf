196b68be39e8a7bbf6eab9a1d816fa69496249ea
Timam <me@timam.io>
2018-05-09 15:33:02 +0600

    Copied from isolated, duplicate stage in module

---
+++ stage-prod-stack/global/s3/main.tf
@@ -0,0 +1,24 @@
+terraform {
+    backend "s3" {
+        encrypt = true
+        bucket = "saif-timam-terraform"
+        region = "us-west-2"
+        key = "terraform.tfstate"
+    }
+}
+
+provider "aws" {
+    region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "saif-timam-terraform"
+
+    versioning {
+        enabled = true
+    }
+
+    lifecycle {
+        prevent_destroy = true
+    }
+}
---
+++ stagiing-prod/stage/data-stores/mysql/main.tf
@@ -0,0 +1,20 @@
+terraform {
+    backend "s3" {
+        encrypt = true
+        bucket = "saif-timam-terraform"
+        region = "us-west-2"
+        key = "stage/data-stores/mysql/terraform.tfstate"
+    }
+}
+
+provider "aws" {
+    region = "us-west-2"
+}
+resource "aws_db_instance" "example" {
+    engine = "mysql"
+    allocated_storage = 10
+    instance_class= "db.t2.micro"
+    name = "example_database"
+    username = "admin"
+    password = "${var.db_password}"
+}
